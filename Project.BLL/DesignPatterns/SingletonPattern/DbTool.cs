ba7fe5bf2df898ab73a678281bae0b2143f50722
hakanakinsu0 <hakanakinsu@outlook.com>
2024-12-26 14:52:58 +0300

    HilalNisa ile BLL katmani uzerinde Singleton design patterni olusturuldu.

---
+++ Project.BLL/DesignPatterns/SingletonPattern/DbTool.cs
@@ -0,0 +1,27 @@
+﻿using Project.DAL.ContextClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.DesignPatterns.SingletonPattern
+{
+    public class DbTool
+    {
+        // Gorev : DbTool sınıfı, uygulama genelinde kullanılan bir singleton tasarım deseni örneğidir. Bu sınıf, MyContext sınıfının bir örneğini oluşturur ve bu örneğin yalnızca bir kez oluşturulmasını garanti eder.
+
+        DbTool() { } // Sınıfın dışarıdan yeni bir örneğinin oluşturulmasını engellemek için private bir kurucu metot tanımlanmıştır.
+
+        static MyContext _dbInstace; // Singleton olarak kullanılacak MyContext örneği için bir private static değişken tanımlanmıştır.
+
+        public static MyContext DbInstace // Singleton örneğine erişim sağlayan public bir static özellik tanımlanmıştır.
+        {
+            get
+            {
+                if (_dbInstace == null) _dbInstace = new MyContext(); // Eğer MyContext örneği henüz oluşturulmamışsa, yeni bir örnek oluşturulup atanır.
+                return _dbInstace; // Her durumda mevcut MyContext örneği döndürülür.
+            }
+        }
+    }
+}
